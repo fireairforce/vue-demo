6d2a6e3115b8993e1f33e38a63cac9413eadeb77
fireairforce <1344492820@qq.com>
2019-09-25 09:09:33 +0800

    feat:add 组件

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,45 @@
 <template>
   <div id="app">
-    <HelloWorld msg="<span style='color:red;'>Hello world</span>"/>
+    <HelloWorld msg="<span style='color:red;'>Hello world</span>" />
     <ev />
+    <!-- 传静态的props是写死的，动态的是活的 -->
+    <com :age="age" @patch="msg" >
+       <h1 slot="a">我要加东西在组件下面</h1>
+       <h2 slot="b">那我要加在里面</h2>
+    </com>  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import ev from './components/events.vue';
-import './components/n';
+import HelloWorld from "./components/HelloWorld.vue";
+import ev from "./components/events.vue";
+import com from "./components/com";
+import "./components/n";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld,
-    ev
+    ev,
+    com
+  },
+  data() {
+    return {
+      age: 18
+    };
+  },
+  methods: {
+    msg: function(b) {
+      window.console.log(b);
+      this.age += 1;
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/com.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div class="com">
+      child components
+    <slot name="b"></slot>
+      {{ name }}
+      {{ age }}
+      <button type="button" name="button" @click="patch">发送到父组件</button>
+      <slot name="a"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["age"],
+  data() {
+    return {
+      name: "com"
+    };
+  },
+  methods:{
+    patch: function(){
+      this.$emit('patch','wdlj');
+    }
+  }
+};
+</script>
+<style>
+</style>
