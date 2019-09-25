c626cf4c81b43ac11e3e0c9814e03873c6408f93
fireairforce <1344492820@qq.com>
2019-09-25 09:38:39 +0800

    feat:add vue-router

---
+++ src/router.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import pageA from "./pages/a.vue";
+import pageB from "./pages/b.vue";
+import App from "./App.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+    { path: "/", component:App },
+    { path: "/pagea", component: pageA },
+    { path: "/pageb", component: pageB }
+];
+
+
+const router = new VueRouter({
+    routes
+})
+
+export default router;
